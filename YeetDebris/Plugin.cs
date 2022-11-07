69bc15962143cb7402789154718383959fa4dfd0
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-07 00:05:28 -0800

    Add project files.

---
+++ YeetDebris/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+
+namespace YeetDebris
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        private const string HarmonyID = "com.steven.yeet.debris";
+
+        private Harmony? _harmonyInstance;
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            _harmonyInstance = new Harmony(HarmonyID);
+            _harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            _harmonyInstance?.UnpatchAll(HarmonyID);
+            _harmonyInstance = null;
+        }
+    }
+}
